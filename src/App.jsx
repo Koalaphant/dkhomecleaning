8cc54e7ac008f70f0a82fc21b1de084410728253
Koalaphant <andrew.wardjones@icloud.com>
2026-04-02 00:52:53 +0100

    feat: implement dynamic page titles based on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./Pages/Home.jsx";
 import About from "./Pages/About.jsx";
 import Contact from "./Pages/Contact.jsx";
@@ -7,6 +8,18 @@ import Footer from "./Components/Footer.jsx";
 import ProgressBar from "./Components/ProgressBar.jsx";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const titles = {
+      "/": "Home",
+      "/about": "About",
+      "/contact": "Contact",
+    };
+    const pageTitle = titles[location.pathname] || "Page";
+    document.title = `DK Home Cleaning | ${pageTitle}`;
+  }, [location.pathname]);
+
   return (
     <>
       <ProgressBar />
